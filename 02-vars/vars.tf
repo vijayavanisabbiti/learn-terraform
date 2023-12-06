6916e19a5b100638dbb405ceecc9fde62f3a0d92
VIJAYAVANI <vijayavanimanju@gmail.com>
2023-12-07 00:09:02 +0530

    Terraform Variables

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -5,3 +5,11 @@ variable "sample1" {
 output "sample1" {
   value = var.sample1
 }
+
+variable "fruits" {
+  default = ["apple", "banana", "orange"]
+}
+
+output "fruits_first_name" {
+  value = var.fruits[0]
+}
