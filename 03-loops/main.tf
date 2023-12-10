cd1b8516220c49d91e5ef971bebb640c7086558f
VIJAYAVANI <vijayavanimanju@gmail.com>
2023-12-10 13:59:19 +0530

    Terraform loops

---
+++ 03-loops/main.tf
@@ -0,0 +1,36 @@
+# Loops in terraform can be done in two ways
+# 1. count
+# 2. for_each
+
+# count is used if your input is list
+# for_each is used if your input is map
+
+
+variable "instances" {
+  default = ["frontend", "backend", "mysql"]
+}
+
+resource "null_resource" "sample" {
+  count = length(var.instances)
+}
+
+variable "instances1" {
+  default = {
+    frontend = {
+      name          = "frontend"
+      instance_type = "t3.micro"
+    }
+    backend = {
+      name          = "backend"
+      instance_type = "t3.micro"
+    }
+    mysql = {
+      name          = "mysql"
+      instance_type = "t3.micro"
+    }
+  }
+}
+
+resource "null_resource" "sample1" {
+  for_each = var.instances1
+}
