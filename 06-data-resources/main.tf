4bd8c7563fcc8e8d73446a81c1f9d4200088e247
VIJAYAVANI <vijayavanimanju@gmail.com>
2023-12-14 14:16:38 +0530

    Terraform - create parameter store

---
+++ 06-data-resources/main.tf
@@ -0,0 +1,19 @@
+#data "aws_ssm_parameter" "user" {
+#  name = "dev.rds.username"
+#}
+#
+#output "username" {
+#  value = data.aws_ssm_parameter.user.value
+#}
+#
+
+data "aws_instance" "instance" {
+  filter {
+    name   = "tag:Name"
+    values = ["workstation"]
+  }
+}
+
+output "instance" {
+  value = "Instance ID - ${data.aws_instance.instance.id} | Public IP - ${data.aws_instance.instance.public_ip}"
+}
